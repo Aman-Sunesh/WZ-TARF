0674769b804ffd521154a14cec4a2a917becfb87
Aman Sunesh <as18181@nyu.edu>
2026-08-11 17:08:48 +0400

    Expose WZTARF model and version in __init__.py

--- src/wztarf/__init__.py
+++ src/wztarf/__init__.py
@@ -1,3 +1,11 @@
-"""WZ-TARF package."""
+"""Expose the main model and package version."""
+
+from .models import WZTARF, WZTARFConfig
 
 __version__ = "0.1.0"
+
+__all__ = [
+    "WZTARF",
+    "WZTARFConfig",
+    "__version__",
+]
