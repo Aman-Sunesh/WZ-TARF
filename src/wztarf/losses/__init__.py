6d7368a973fd8501c1343c1489fbdd6650917aad
Aman Sunesh <as18181@nyu.edu>
2026-08-11 16:05:26 +0400

    Refine docstring and update __all__ exports
    Updated module docstring to clarify purpose and added supervised loss components to the __all__ export list.

--- src/wztarf/losses/__init__.py
+++ src/wztarf/losses/__init__.py
@@ -1,1 +1,44 @@
-"""Supervised WZ-TARF loss components."""
+"""Expose the supervised loss components used to train WZ-TARF."""
+
+from .classification import classification_loss, soft_quality_targets
+from .directional import directional_loss
+from .diversity import diversity_loss
+from .dynamics import dynamics_loss
+from .endpoint import endpoint_loss
+from .lane_goal import lane_goal_loss
+from .refinement import refinement_loss
+from .road_compliance import road_compliance_loss
+from .route import route_loss
+from .supervised import (
+    LossWeights,
+    SupervisedLossOutput,
+    supervised_loss,
+)
+from .trajectory import (
+    mode_assignment_cost,
+    trajectory_loss,
+    winner_indices,
+)
+from .worker_clearance import worker_clearance_loss
+from .workzone_geometry import workzone_geometry_loss
+
+__all__ = [
+    "mode_assignment_cost",
+    "winner_indices",
+    "trajectory_loss",
+    "endpoint_loss",
+    "classification_loss",
+    "soft_quality_targets",
+    "lane_goal_loss",
+    "route_loss",
+    "directional_loss",
+    "dynamics_loss",
+    "diversity_loss",
+    "road_compliance_loss",
+    "workzone_geometry_loss",
+    "worker_clearance_loss",
+    "refinement_loss",
+    "LossWeights",
+    "SupervisedLossOutput",
+    "supervised_loss",
+]
