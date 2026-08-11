fb34ba62f1c88c734ea236f37d1c43782ba34c50
Aman Sunesh <as18181@nyu.edu>
2026-08-11 16:58:17 +0400

    Update module docstring and exports

--- src/wztarf/models/route/__init__.py
+++ src/wztarf/models/route/__init__.py
@@ -1,1 +1,9 @@
-"""Graph-goal and route-query modules."""
+"""Expose graph-route queries and beyond-map continuation prediction."""
+
+from .goal_queries import RouteGoalQueries
+from .map_exit import MapExitGoalHead
+
+__all__ = [
+    "RouteGoalQueries",
+    "MapExitGoalHead",
+]
