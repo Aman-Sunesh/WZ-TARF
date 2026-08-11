90af8d77db93705a84070a4fe17320884f4aa026
Aman Sunesh <as18181@nyu.edu>
2026-08-11 18:38:28 +0400

    Add TopologyTargets to __all__ exports

--- src/wztarf/pretraining/__init__.py
+++ src/wztarf/pretraining/__init__.py
@@ -11,13 +11,19 @@ from .masking import (
     build_mask_plan,
 )
 from .topology_reconstruction import topology_reconstruction_loss
+from .topology_targets import (
+    TopologyTargets,
+    build_topology_targets,
+)
 
 __all__ = [
     "MaskingConfig",
     "MaskPlan",
+    "TopologyTargets",
     "build_mask_plan",
     "masked_reconstruction_loss",
     "build_false_negative_mask",
     "future_contrastive_loss",
     "topology_reconstruction_loss",
+    "build_topology_targets",
 ]
