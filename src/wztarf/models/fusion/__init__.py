54fbbccba6fc0b739688e22af47bb92ffe38a712
Aman Sunesh <as18181@nyu.edu>
2026-08-11 16:56:32 +0400

    Update module docstring and exports

--- src/wztarf/models/fusion/__init__.py
+++ src/wztarf/models/fusion/__init__.py
@@ -1,1 +1,9 @@
-"""Role-aware fusion modules."""
+"""Expose control and horizon-adaptive scene fusion modules."""
+
+from .control_fusion import ControlFusion
+from .horizon_fusion import HorizonFusion
+
+__all__ = [
+    "ControlFusion",
+    "HorizonFusion",
+]
