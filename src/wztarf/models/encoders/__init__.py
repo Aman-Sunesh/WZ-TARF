9bf5a5877dcc9c37040dc33a3252e6ee10d11b0d
Aman Sunesh <as18181@nyu.edu>
2026-08-11 16:53:33 +0400

    Update __init__.py to expose scene encoders

--- src/wztarf/models/encoders/__init__.py
+++ src/wztarf/models/encoders/__init__.py
@@ -1,1 +1,17 @@
-"""Role-specific scene encoders."""
+"""Expose the role-specific scene encoders used by WZ-TARF."""
+
+from .agent_encoder import AgentEncoder
+from .control_encoder import ControlEncoder
+from .gaze_encoder import GazeEncoder
+from .lane_encoder import LaneEncoder
+from .motion_encoder import MotionEncoder
+from .workzone_encoder import WorkZoneEncoder
+
+__all__ = [
+    "MotionEncoder",
+    "ControlEncoder",
+    "GazeEncoder",
+    "AgentEncoder",
+    "LaneEncoder",
+    "WorkZoneEncoder",
+]
